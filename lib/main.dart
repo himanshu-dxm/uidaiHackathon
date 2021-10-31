7604790e32279ccf4f11c91d31b7a78a9cd1500e
AmithShubhan <amithshubhan0@gmail.com>
2021-10-31 14:45:25 +0530

    amiths commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'dart:math';
 
 import 'package:aadharupdater/models/Auth.dart';
@@ -6,30 +5,30 @@ import 'package:aadharupdater/screens/authenticateOperator.dart';
 import 'package:aadharupdater/screens/home.dart';
 import 'package:aadharupdater/utils/operatorauth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'screens/demoScreen.dart';
 
-Future<void> main()async{
-    WidgetsFlutterBinding.ensureInitialized();
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    // prefs.clear();//need to be commented
-    var Is = prefs.containsKey('OpID');
-    var s = prefs.getString('OpID');
-    if(!Is || s==null || s.length ==0 )
-    {
-      MyApp.isAuth = false;
-    }
-    else{
-      MyApp.isAuth = true;
-    }
-    runApp(MyApp());
-    await MyHomePageState.getLocation();// initially get the location
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  // prefs.clear();//need to be commented
+  var Is = prefs.containsKey('OpID');
+  var s = prefs.getString('OpID');
+  if (!Is || s == null || s.length == 0) {
+    MyApp.isAuth = false;
+  } else {
+    MyApp.isAuth = true;
+  }
+  runApp(MyApp());
+  await MyHomePageState.getLocation(); // initially get the location
 
   // print("Inside main()");
   // Future<http.Response> res = Authentication.sendOTP("999936311166", "0acbaa8b-b3ae-433d-a5d2-51250ea8e970");
-  }
+}
+
 class MyApp extends StatelessWidget {
   static bool isAuth = false;
   @override
@@ -37,9 +36,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Aadhar Updater',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primaryColor: Colors.orange),
+      theme: ThemeData(primaryColor: Colors.orange).copyWith(
+        textTheme: GoogleFonts.robotoTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
       // home: AuthOperatorScreen(),
-      home: isAuth?AuthOperatorScreen():OpAuth(),
+      home: isAuth ? AuthOperatorScreen() : OpAuth(),
     );
   }
 }
--- lib/utils/operatorauth.dart
+++ lib/utils/operatorauth.dart
@@ -2,40 +2,60 @@ import 'package:aadharupdater/screens/authenticateOperator.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class OpAuth extends StatefulWidget{
+class OpAuth extends StatefulWidget {
   OpAuthState createState() => OpAuthState();
 }
 
-class OpAuthState extends State<OpAuth>{
+class OpAuthState extends State<OpAuth> {
   TextEditingController opID = new TextEditingController();
   bool isLoading = false;
-  Widget build(context){
-    return isLoading?Center(child: CircularProgressIndicator(),):Scaffold(
-      appBar: AppBar(title: Text("Register Operator's UID"),),
-      body: Container(
-        decoration: BoxDecoration(border: Border.all(width: 1)),
-        padding: EdgeInsets.all(3),
-        margin: EdgeInsets.all(3),
-        child: Column(children: [
-          TextField(
-            controller: opID,
-            decoration: InputDecoration(hintText: 'ID/NAME',border: OutlineInputBorder()),
-          ),
-          ElevatedButton(onPressed: ()async{
-            //send otp
-            setState(() {
-              isLoading = true;
-            });
-            var prefs = await SharedPreferences.getInstance();
-                prefs.setString('OpID',opID.text.toString());
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("saved")));
-            setState(() {
-              isLoading = false;
-            });
-            Navigator.pushReplacement(context,MaterialPageRoute(builder: (context)=>AuthOperatorScreen()));
-          }, child: Text("Save")),
-        ],),
-      ),
-    );
+  Widget build(context) {
+    return isLoading
+        ? Center(
+            child: CircularProgressIndicator(),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              shape: ContinuousRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(30),
+                      bottomRight: Radius.circular(100))),
+              backgroundColor: Colors.black,
+              title: Text("Register Operator's UID"),
+            ),
+            body: Container(
+              decoration: BoxDecoration(border: Border.all(width: 1)),
+              padding: EdgeInsets.all(3),
+              margin: EdgeInsets.all(3),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: opID,
+                    decoration: InputDecoration(
+                        hintText: 'ID/NAME', border: OutlineInputBorder()),
+                  ),
+                  ElevatedButton(
+                      onPressed: () async {
+                        //send otp
+                        setState(() {
+                          isLoading = true;
+                        });
+                        var prefs = await SharedPreferences.getInstance();
+                        prefs.setString('OpID', opID.text.toString());
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(SnackBar(content: Text("saved")));
+                        setState(() {
+                          isLoading = false;
+                        });
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => AuthOperatorScreen()));
+                      },
+                      child: Text("Save")),
+                ],
+              ),
+            ),
+          );
   }
 }
